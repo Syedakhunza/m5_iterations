4001051dfc21e94afef016efe52cd8351db28ac2
Syedakhunza <slnu3@my.nl.edu>
2024-10-22 15:34:02 -0500

    Add files via upload

--- m5.py.3file.py
+++ m5.py.3file.py
@@ -1,7 +1,7 @@
 #syeda khunza fatima
 #22nd oct,2024
 
-
+#this program help us in iterates the integers
 for num in range (1, 51):
     if num %3 == 0 and num %5 == 0:
         print ("divisible by both")
